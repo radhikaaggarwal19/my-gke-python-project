9ce45da9b304e3eb9b37d1095fee0c6a7a6efc8a
Radhika Aggarwal <radhikaxaggarwal@gmail.com>
2025-04-30 14:14:43 -0500

    Reverted back to CI and CD

--- app/main.py
+++ app/main.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "My Python App on GKE has been updated via CI/CD v2!"
+    return "My Python App on GKE has been updated via CI and CD!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     # Run on 0.0.0.0 to be accessible within Docker later
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)œ
